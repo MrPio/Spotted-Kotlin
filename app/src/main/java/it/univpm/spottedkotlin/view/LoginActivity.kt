6eb01d93733e8b3eaccde48785234dca5edcb0c0
SbattellaMattia <tiasba01@gmail.com>
2023-03-28 18:22:09 +0200

    Organization changes (2)

--- app/src/main/java/it/univpm/spottedkotlin/view/LoginActivity.kt
+++ app/src/main/java/it/univpm/spottedkotlin/view/LoginActivity.kt
@@ -2,12 +2,12 @@ package it.univpm.spottedkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import it.univpm.spottedkotlin.R
+
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.first_access_login)
+        setContentView(R.layout.login_activity)
     }
 }
