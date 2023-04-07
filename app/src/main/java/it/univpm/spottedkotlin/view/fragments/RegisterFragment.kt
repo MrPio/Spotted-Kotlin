6ea29ef89700598abdd7350c7a77bd5f8dc482e1
SbattellaMattia <tiasba01@gmail.com>
2023-04-07 12:53:40 +0200

    new password strong and fix

--- app/src/main/java/it/univpm/spottedkotlin/view/fragments/RegisterFragment.kt
+++ app/src/main/java/it/univpm/spottedkotlin/view/fragments/RegisterFragment.kt
@@ -1,22 +1,23 @@
 package it.univpm.spottedkotlin.view.fragments
 
 import android.content.ContentValues.TAG
-import android.inputmethodservice.KeyboardView
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.OnKeyListener
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import it.univpm.spottedkotlin.R
-import it.univpm.spottedkotlin.databinding.LoginFragmentBinding
 import it.univpm.spottedkotlin.databinding.RegisterFragmentBinding
+import java.io.Console
 
 
 class RegisterFragment : Fragment() {
@@ -27,11 +28,11 @@ class RegisterFragment : Fragment() {
     private val character : String = "abcdefghijklmnopqrstuvwxyz"
     private val character_up : String = character.uppercase()
     private val number : String = "0123456789"
-    private val special : String = "!\"#\$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    private val special : String = "!\"#\$€%&'()*+,-./:;<=>?@[\\]^_`{|}~"
 
     //Almost 6 character
     private val MIN_LENGHT : Int = 6
-    private var strong : Int = 0
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,16 +71,29 @@ class RegisterFragment : Fragment() {
 
 
         //TODO(Barra per la password che cambia colore)
-        /*binding.RegisterPassword.setOnClickListener(){
+        binding.RegisterPassword.addTextChangedListener(object : TextWatcher{
 
-            val x = OnKeyListener()
-            Thread(Runnable {
-                while(true) {
-                    //validation(binding.RegisterPassword.text.toString(),"")
-                    binding.button222.setBackgroundColor(0x000000)
+            override fun beforeTextChanged(charSequence : CharSequence, i : Int, i1 : Int, i2 : Int) {}
+
+            override fun onTextChanged(charSequence : CharSequence, i : Int, i1 : Int, i2 : Int) {
+                // Recupera il testo dalla casella di testo
+                val input : String = binding.RegisterPassword.getText().toString()
+
+                when (pass_strong(input)) {
+                    0 -> binding.button222.setBackgroundColor(Color.TRANSPARENT)
+                    1 -> binding.button222.setBackgroundColor(Color.RED)
+                    2 -> binding.button222.setBackgroundColor(Color.YELLOW)
+                    3 -> binding.button222.setBackgroundColor(Color.YELLOW)
+                    4 -> binding.button222.setBackgroundColor(Color.GREEN)
+                    else -> { // Note the block
+                        binding.button222.setBackgroundColor(Color.TRANSPARENT)
+                    }
                 }
-            }).start()
-        }*/
+            }
+
+            override fun afterTextChanged(editable : Editable) {}
+            })
+
 
         binding.doLoginText.setOnClickListener {
             binding.root.findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
@@ -102,23 +116,44 @@ class RegisterFragment : Fragment() {
         var checkNumber : Boolean = false
         var checkSpecial : Boolean = false
 
-        for (i in 0..caratteri.size){
-            if (!checkChar){if(caratteri[i] in character) checkChar=true; strong+=1}
-            if (!checkCharUp){if(caratteri[i] in character_up) checkCharUp=true; strong+=1}
-            if (!checkNumber){if(caratteri[i] in number) checkNumber=true; strong+=1}
-            if (!checkSpecial){if(caratteri[i] in special) checkSpecial=true; strong+=1}
+        for (i in 0..(caratteri.size-1)){
+            if (!checkChar){if(caratteri[i] in character) checkChar=true}
+            if (!checkCharUp){if(caratteri[i] in character_up) checkCharUp=true}
+            if (!checkNumber){if(caratteri[i] in number) checkNumber=true}
+            if (!checkSpecial){if(caratteri[i] in special) checkSpecial=true}
         }
-        /*
-        0-No input
-        1-Weak
-        2-Normal
-        3-Strong
-        4-VeryStrong
-        */
 
         if(pass.length < MIN_LENGHT) return false
         if(pass != rep_pass) return false
         return checkChar && checkCharUp && checkNumber && checkSpecial
+    }
+
+    /*
+     0-No input
+     1-Weak
+     2-Normal
+     3-Strong
+     4-VeryStrong
+     */
+    fun pass_strong(pass: String): Int {
+        val caratteri = pass.toCharArray()
+        var strong : Int = 0
+
+        //For password check
+        var checkChar : Boolean = false
+        var checkCharUp : Boolean = false
+        var checkNumber : Boolean = false
+        var checkSpecial : Boolean = false
+
+        for (i in 0..caratteri.size-1){
+            if (!checkChar){if(caratteri[i] in character) {checkChar=true; strong++}}
+            if (!checkCharUp){if(caratteri[i] in character_up){checkCharUp=true; strong++}}
+            if (!checkNumber){if(caratteri[i] in number) {checkNumber=true; strong++}}
+            if (!checkSpecial){if(caratteri[i] in special) {checkSpecial=true; strong++}}
+        }
+        Toast.makeText(context, strong.toString(),
+            Toast.LENGTH_SHORT).show()
+        return strong
     }
 
 
