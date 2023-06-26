51d1c35f0b656ab062ba03a36207e2db9fe4e2e0
SbattellaMattia <tiasba01@gmail.com>
2023-06-26 14:58:11 +0200

    change image button

--- app/src/main/java/it/univpm/spottedkotlin/viewmodel/AccountViewModel.kt
+++ app/src/main/java/it/univpm/spottedkotlin/viewmodel/AccountViewModel.kt
@@ -2,11 +2,38 @@ package it.univpm.spottedkotlin.viewmodel
 
 import androidx.lifecycle.ViewModel
 import it.univpm.spottedkotlin.managers.AccountManager
+import it.univpm.spottedkotlin.managers.DatabaseManager
+import it.univpm.spottedkotlin.model.Post
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 
 class AccountViewModel : ViewModel() {
 
     var name: String = AccountManager.user.name +" "+ AccountManager.user.surname
-    lateinit var nameInsta:String
+    var nameInsta:String=setNameInsta()
+    var numPost:String=setNumPost()
+    var numFollowing:String=setNumFollowing()
+    var numComment:String=setNumComment()
 
-	// TODO: Implement the ViewModel
+    fun setNameInsta(): String{
+        if(AccountManager.user.instagramNickname != null){
+            return AccountManager.user.instagramNickname!!
+        }
+        else return ""
+    }
+
+    fun setNumPost():String{
+        return AccountManager.user.posts.size.toString()
+    }
+
+    fun setNumFollowing(): String {
+        return AccountManager.user.following.size.toString()
+    }
+
+    fun setNumComment(): String {
+        return AccountManager.user.comments.size.toString()
+    }
+
+    // TODO: Implement the ViewModel
 }
