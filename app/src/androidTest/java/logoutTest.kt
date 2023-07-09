cb6ba52002bbbbd853353618a5f456db57f475fd
MrPio <valeriomorelli50@gmail.com>
2023-07-09 11:10:27 +0200

    Login/Logout espresso test

--- app/src/androidTest/java/logoutTest.kt
+++ app/src/androidTest/java/logoutTest.kt
@@ -4,38 +4,59 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import it.univpm.spottedkotlin.R
 import it.univpm.spottedkotlin.managers.AccountManager
+import it.univpm.spottedkotlin.managers.DataManager
 import it.univpm.spottedkotlin.managers.SeederManager
 import it.univpm.spottedkotlin.view.MainActivity
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.regex.Pattern.matches
 
 
 @RunWith(AndroidJUnit4::class)
 class HelloWorldEspressoTest {
 
 	@Before
-	fun initialize(){
-		AccountManager.user=SeederManager.defaultUser
+	fun initialize() {
+		AccountManager.user = SeederManager.defaultUser
+		Intents.init();
 	}
 
-//	@get:Rule
-//	val activityRule = ActivityScenarioRule(MainActivity::class.java)
-
 	@Test
-	fun gotoSettings() {
-		// Launch the activity
-		val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-		// Perform action: Click the button
-		Espresso.onView(ViewMatchers.withId(R.id.home_menu)).perform(ViewActions.click())
-		activityScenario.close()
+	fun logoutAndLoginTest() {
+		runBlocking {
+
+			// Launch the activity
+			val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+			DataManager.fetchData(InstrumentationRegistry.getInstrumentation().targetContext)
+
+			// LOGOUT
+			Espresso.onView(withId(R.id.home_menu)).perform(ViewActions.click())
+			Espresso.onView(withText("Logout")).perform(ViewActions.click())
+			Espresso.onView(withText("Si")).perform(ViewActions.click())
+
+			// LOGIN
+			Espresso.onView(withText("Iniziamo")).perform(ViewActions.click())
+			Espresso.onView(withId(R.id.TextEmail))
+				.perform(ViewActions.typeText("valeriomorelli50@gmail.com"))
+			Espresso.onView(withId(R.id.TextPassword)).perform(ViewActions.typeText("password"))
+
+			// GOTO MAIN ACTIVITY
+			Espresso.onView(withId(R.id.login_button)).perform(ViewActions.click())
+			Intents.intended(hasComponent(hasClassName(MainActivity::class.java.name)))
+			Thread.sleep(2000)
+			Intents.release()
+			// CHECK
+			Espresso.onView(withText("Ciao, Valerio!")).check(ViewAssertions.matches(isDisplayed()))
+			activityScenario.close()
+		}
 	}
 }
