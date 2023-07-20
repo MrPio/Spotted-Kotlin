0cbec22ade9f65dd58bbfa367fac35044fa65e5d
MrPio <valeriomorelli50@gmail.com>
2023-07-21 00:53:09 +0200

    SearchUserFragment several fixes

--- app/src/main/java/it/univpm/spottedkotlin/view/fragments/SearchUserFragment.kt
+++ app/src/main/java/it/univpm/spottedkotlin/view/fragments/SearchUserFragment.kt
@@ -7,9 +7,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import it.univpm.spottedkotlin.adapter.SearchUserAdapter
 import it.univpm.spottedkotlin.databinding.SearchUserFragmentBinding
+import it.univpm.spottedkotlin.extension.function.getActivity
+import it.univpm.spottedkotlin.view.MainActivity
 import it.univpm.spottedkotlin.viewmodel.SearchUserViewModel
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class SearchUserFragment(val uid: String? = null) : Fragment() {
 
@@ -20,16 +25,24 @@ class SearchUserFragment(val uid: String? = null) : Fragment() {
 		inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
 	): View {
 		binding = SearchUserFragmentBinding.inflate(inflater, container, false)
-		binding.searchUserRecycler.layoutManager=
+		binding.searchUserRecycler.layoutManager =
 			LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+		binding.searchUserRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+			override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+				super.onScrolled(recyclerView, dx, dy)
+				context?.getActivity<MainActivity>()?.binding?.bottomBarContainer?.translationY =
+					binding.searchUserRecycler.computeVerticalScrollOffset().toFloat()
+			}
+		})
+
 		binding.viewModel = viewModel
 		return binding.root
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
-		viewModel.users.observe(requireActivity()){
-			binding.searchUserRecycler.adapter=SearchUserAdapter(it)
+		viewModel.users.observe(requireActivity()) {
+			binding.searchUserRecycler.adapter = SearchUserAdapter(it)
 		}
 	}
 }
--- app/src/main/java/it/univpm/spottedkotlin/viewmodel/SearchUserViewModel.kt
+++ app/src/main/java/it/univpm/spottedkotlin/viewmodel/SearchUserViewModel.kt
@@ -2,11 +2,13 @@ package it.univpm.spottedkotlin.viewmodel
 
 import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
+import it.univpm.spottedkotlin.BR
 import it.univpm.spottedkotlin.extension.ObservableViewModel
 import it.univpm.spottedkotlin.managers.DataManager
 import it.univpm.spottedkotlin.model.User
 
 class SearchUserViewModel : ObservableViewModel() {
+	@get:Bindable
 	val users = MutableLiveData(listOf<User>())
 
 	@get:Bindable
@@ -18,6 +20,9 @@ class SearchUserViewModel : ObservableViewModel() {
 					(it.name + it.surname + it.name + it.instagramNickname).lowercase()
 						.contains(value.lowercase())
 				}
+			else
+				users.value= listOf()
+			notifyPropertyChanged(BR.users)
 		}
 }
 
